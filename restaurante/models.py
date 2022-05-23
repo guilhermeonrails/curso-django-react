ae5ac95410a46d564c319fbedd12a31a1ce804fa
Guilherme Lima <guilherme.lima@alura.com>
2022-05-23 19:24:15 -0300

    img returns

--- restaurante/models.py
+++ restaurante/models.py
@@ -4,7 +4,7 @@ class Prato(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     preco = models.DecimalField(decimal_places=2, max_digits=18)
-    imagem = models.URLField(blank=True)
+    imagem = models.ImageField(blank=True)
 
     def __str__(self):
         return self.nome
