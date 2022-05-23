6db9c1aa86b631ea1e5ab2b6fa76d1d3582880a2
Guilherme Lima <guilherme.lima@alura.com>
2022-05-23 19:27:41 -0300

    url wins

--- restaurante/models.py
+++ restaurante/models.py
@@ -4,7 +4,7 @@ class Prato(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     preco = models.DecimalField(decimal_places=2, max_digits=18)
-    imagem = models.ImageField(blank=True)
+    imagem = models.URLField(blank=True)
 
     def __str__(self):
         return self.nome
